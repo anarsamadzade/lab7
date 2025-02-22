5646cee0c994f43bc0c28df6adda04814debdabc
Anar Samadzade <your-email@example.com>
2025-02-21 22:08:48 -0500

    Initial Commit

---
+++ LabExercise7Tests/LabExercise7Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LabExercise7Tests.swift
+//  LabExercise7Tests
+//
+//  Created by Tech Atro on 2025-02-21.
+//
+
+import Testing
+@testable import LabExercise7
+
+struct LabExercise7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
